840be6eabd97db2c477c0fc17dc7a743ef033a60
Jakub <jtomgluba@gmail.com>
2018-09-15 16:05:34 +0200

    landing page parralax

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Authentication from '@/components/pages/Authentication/Authentication.vue'
+import Home from '@/components/pages/home/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  data: {
+    currentRoute: window.location.pathname
+  },
   routes: [
     {
+      path: '/login',
+      name: 'Authentication',
+      component: Authentication,
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home,
     }
   ]
 })
