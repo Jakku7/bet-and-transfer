159a33645bf82d4380920d897c1cc99b16a52fbc
Jakub <jtomgluba@gmail.com>
2018-09-15 16:06:49 +0200

    packages

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,40 +1,12 @@
 <template>
-  <v-app>
+  <div id="app">
     <v-container>
       <router-view/>
     </v-container>
-  </v-app>
+  </div>
 </template>
-
 <script>
   export default {
-    name: 'app',
-    data () {
-      return {}
-    }
+    name: 'app'
   }
 </script>
-
-<style lang="scss">
-  @import "./assets/styles";
-
-  body {
-    background: url('./assets/images/background.jpg') no-repeat center center fixed;
-    background-size: cover;
-    &:after {
-      content: '';
-      position: fixed;
-      width: 100%;
-      height: 100%;
-      top: 0;
-      left: 0;
-      background-color: $background-tint;
-      opacity: .3;
-      z-index: -1;
-    }
-
-    .application {
-      background: none;
-    }
-  }
-</style>
